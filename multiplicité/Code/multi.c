32a9cacc054d6baccd1064ca2c65605bfe810af2
max93der <maxime.deravet.md@gmail.com>
2021-03-26 11:52:30 +0100

    Révision du style et espacement à 3 espaces

--- multiplicité/Code/multi.c
+++ multiplicité/Code/multi.c
@@ -25,21 +25,21 @@ int multiplicite(int *T, const int N, int *max){
       ////Nombre le plus grand entre les deux valeurs actuelles
 
       if (i!=j){
-        if (nbri > nbrj){
-          temp = nbri;
-        }//fin if i>j
+         if (nbri > nbrj){
+            temp = nbri;
+         }//fin if i>j
 
-        else{
-          temp = nbrj;
-        }//fin else
-     }
-     else
-     temp = nbri;
+         else{
+            temp = nbrj;
+         }//fin else
+      }
+      else
+      temp = nbri;
 
 
      ////Changement du maximum et du nombre d'occurence
 
-     if (i ==0){ //Premier tour de boucle
+      if (i ==0){ //Premier tour de boucle
          maxi = temp;
          if ((nbri == nbrj)&&(i!=j))
             occurence = 2;
@@ -47,36 +47,36 @@ int multiplicite(int *T, const int N, int *max){
             occurence = 1;
       }//fin if i ==0
 
-     if (maxi < temp){ //si le nouveau nombre est plus grand, changement et reinitialisation de l'occurences
-       maxi = temp;
-       if ((nbri == nbrj)&&(i!=j)) // si les deux valeurs sont égale et qu'on regarde deux valeur différente => (Occurence ==2)  sinon occurence ==1
-          occurence = 2;
-       else
-          occurence = 1;
-     }//fin if max<temp
+      if (maxi < temp){ //si le nouveau nombre est plus grand, changement et reinitialisation de l'occurences
+         maxi = temp;
+         if ((nbri == nbrj)&&(i!=j)) // si les deux valeurs sont égale et qu'on regarde deux valeur différente => (Occurence ==2)  sinon occurence ==1
+            occurence = 2;
+         else
+            occurence = 1;
+      }//fin if max<temp
 
 
-     else if ((maxi == temp) && (i!=0)){//Si le nouveau nombre est égal, augmentation de l'occurence
+      else if ((maxi == temp) && (i!=0)){//Si le nouveau nombre est égal, augmentation de l'occurence
 
-        if ((nbri == nbrj)&&(i!=j)) // si les deux valeurs sont égale et qu'on regarde deux valeur différente => (Occurence +2)  sinon occurence +1
-           occurence += 2;
-        else
-           occurence += 1;
+         if ((nbri == nbrj)&&(i!=j)) // si les deux valeurs sont égale et qu'on regarde deux valeur différente => (Occurence +2)  sinon occurence +1
+            occurence += 2;
+         else
+            occurence += 1;
       }//fin if maxi == temp
 
 
-   //incrémentation des compteurs
-     i++;
-     j--;
+    //incrémentation des compteurs
+      i++;
+      j--;
 
 
 
 
 
- }//fin while
+   }//fin while
 
- *max = maxi;
+   *max = maxi;
 
- return occurence;
+   return occurence;
 
 }//fin multiplicite
