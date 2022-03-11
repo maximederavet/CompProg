4685bd3a7f868b759aeb9b63dfd43f0dae68cecf
max93der <maxime.deravet.md@gmail.com>
2022-03-11 14:34:26 +0100

    Initial Commit 2022

---
+++ Filtrer/code/propriete.h
@@ -0,0 +1,20 @@
+/**
+ * \file propriete.h
+ * \brief Header pour la propriété p
+ * \author Benoit Donnet, Géraldine Brieven -- Université de Liège (ULiège)
+ * \version 0.1
+ * \date 16/11/2021
+ *
+ * Implémente la propriété p().
+ */
+
+#ifndef __PROPRIETE__
+#define __PROPRIETE__
+
+/*
+ * @pre: /
+ * @post: test = p(x)
+ */
+int test(int x);
+
+#endif
