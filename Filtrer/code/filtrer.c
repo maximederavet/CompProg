3c129d7842175cde618134ea5206ce1f59629870
lucamat12113 <colosus6015@gmail.com>
2022-03-30 23:36:12 +0200

    début commentaires

--- Filtrer/code/filtrer.c
+++ Filtrer/code/filtrer.c
@@ -6,12 +6,25 @@
 
 int filtrer(int *T, int N){
     assert(T != NULL && N >0);
-
+    //∀ T[i], 0 ≤ i ≤ N, T[i] ∈ Z (Z = les entiers)  ∧  N > 0
     int i= 0;
     int taille_utile = 0;
-    
-
+    //∀ T[i], 0 ≤ i ≤ N, T[i] ∈ Z (Z = les entiers)  ∧  N > 0  ∧ i=0  ∧  taille_utile=0
+    /*N = N_initial  
+      ∧
+      0 ≤ taille_utile ≤ i < N
+      ∧  
+      ∀ x, 0 ≤ x < taille_utile, Filtrage(T_initial, N, taille_utile), Sous_Suite(T_initial, taille_utile, T, N)
+    */
     while(i<N){
+    /*N = N_initial  
+      ∧
+      0 ≤ taille_utile ≤ i < N
+      ∧  
+      ∀ x, 0 ≤ x < taille_utile, Filtrage(T_initial, N, taille_utile), Sous_Suite(T_initial, taille_utile, T, N)
+      ∧
+      i < N
+    */        
         if(!test(T[i])){
             T[i]= 0;
             i++;
@@ -29,8 +42,8 @@ int filtrer(int *T, int N){
             }
         }
 
-    }//fin while
+    }
 
     return taille_utile;
 
-}//fin filtrer
+}
