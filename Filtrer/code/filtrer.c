b36671a6357b00982c816bf4a3a6fc5ec99113ab
lucamat12113 <colosus6015@gmail.com>
2022-03-30 22:57:19 +0200

    ok j'ai réparé ma merde

--- Filtrer/code/filtrer.c
+++ Filtrer/code/filtrer.c
@@ -9,21 +9,13 @@ int filtrer(int *T, int N){
 
     int i= 0;
     int taille_utile = 0;
-<<<<<<< HEAD
     
-=======
-
->>>>>>> e41cec8a1960c3940ecfe4babcebd71a46715076
 
     while(i<N){
         if(!test(T[i])){
             T[i]= 0;
             i++;
-<<<<<<< HEAD
             
-=======
-
->>>>>>> e41cec8a1960c3940ecfe4babcebd71a46715076
 
         }else{
             if(taille_utile!= i){
