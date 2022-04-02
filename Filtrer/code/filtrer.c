78f6dba253d01c6bf33b2c5692cbf9c760b9d770
lucamat12113 <colosus6015@gmail.com>
2022-04-02 11:57:00 +0200

    on met les coms sur overleaf

--- Filtrer/code/filtrer.c
+++ Filtrer/code/filtrer.c
@@ -6,25 +6,12 @@
 
 int filtrer(int *T, int N){
     assert(T != NULL && N >0);
-    //∀ T[i], 0 ≤ i ≤ N, T[i] ∈ Z (Z = les entiers)  ∧  N > 0
+    
     int i= 0;
     int taille_utile = 0;
-    //∀ T[i], 0 ≤ i ≤ N, T[i] ∈ Z (Z = les entiers)  ∧  N > 0  ∧ i=0  ∧  taille_utile=0
-    /*N = N_initial  
-      ∧
-      0 ≤ taille_utile ≤ i < N
-      ∧  
-      ∀ x, 0 ≤ x < taille_utile, Filtrage(T_initial, N, taille_utile), Sous_Suite(T_initial, taille_utile, T, N)
-    */
+    
     while(i<N){
-    /*N = N_initial  
-      ∧
-      0 ≤ taille_utile ≤ i < N
-      ∧  
-      ∀ x, 0 ≤ x < taille_utile, Filtrage(T_initial, N, taille_utile), Sous_Suite(T_initial, taille_utile, T, N)
-      ∧
-      i < N
-    */        
+    
         if(!test(T[i])){
             T[i]= 0;
             i++;
@@ -45,10 +32,5 @@ int filtrer(int *T, int N){
     }
 
     return taille_utile;
-    /*N = N_initial
-      ∧
-      filtrer(T, N)= taille_utile  
-      ∧
-      T=[ (  Sous_Suite(T_initial, taille_utile, T, N) ∧ Filtrage(T_initial, N, taille_utile)  ) || Zone_Morte(T, N, taille_utile)  ]
-    */
+    
 }
